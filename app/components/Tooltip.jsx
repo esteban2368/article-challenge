7c1f4bfae6d6d6839e079f50657b661348040da7
esteban2368 <esteban.d2168@gmail.com>
2023-12-19 23:18:31 -0500

    add styles tootip on desktop and add event toggle tooltip mobile

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -31,14 +31,40 @@ const useBreakPoint = (width) => {
 const Tooltip = ( { text, icons, trigger, children } ) =>{
     const [isVisible, setIsVisible] = useState(false)
     const breakPoint = useBreakPoint(640)
+
+    const toggleShowTooltip = () => {
+        setIsVisible(!isVisible)
+    }
+
     return (
         <div>
             {breakPoint ? (
                 <>
-                    <button className="rounded-full w-8 h-8 p-2 bg-slate-100 hover:bg-slate-500 relative z-40">{children}</button>
-                    <div className={`${StyleTooltip.tooltip__container} ${StyleTooltip.tooltip__containerMobile} flex gap-5 items-center z-10 py-5 px-8`}>
-                        <span className='uppercase'>{text}</span>
-                        <div className='flex gap-5'>
+                    <button className="flex rounded-full w-8 h-8 p-2 bg-slate-100 text-[var(--dark-blue)] hover:bg-slate-500 hover:text-white" onClick={toggleShowTooltip}>{children}</button>
+                    {isVisible && (
+                        <div className={`${StyleTooltip.tooltip__container} ${StyleTooltip.tooltip__containerMobile} flex items-center py-5 px-8`}>
+                            <span className='uppercase mr-5'>{text}</span>
+                            <div className='flex gap-4'>
+                                <a href='https://www.facebook.com/' target='_blank'>
+                                    <FontAwesomeIcon icon={faSquareFacebook} className='text-white' size='xl'/>
+                                </a>
+                                <a href='https://twitter.com/?lang=es' target='_blank'>
+                                    <FontAwesomeIcon icon={faTwitter} className='text-white' size='xl'/>
+                                </a>
+                                <a href='https://co.pinterest.com/' target='_blank'>
+                                    <FontAwesomeIcon icon={faPinterest} className='text-white' size='xl'/>
+                                </a>
+                            </div>
+                            <button className="rounded-full w-8 h-8 p-2 ml-auto text-white bg-slate-500" onClick={toggleShowTooltip}>{children}</button>
+                        </div>
+                    )}
+                </>
+                ) : (
+                <>
+                    <button className="flex rounded-full w-8 h-8 p-2 bg-slate-100 text-[var(--dark-blue)] hover:bg-slate-500 hover:text-white">{children}</button>
+                    <div className={`${StyleTooltip.tooltip__container} ${StyleTooltip.tooltip__containerDesktop} flex items-center rounded-lg py-5 px-8`}>
+                    <span className='uppercase mr-5'>{text}</span>
+                        <div className='flex gap-4'>
                             <a href='https://www.facebook.com/' target='_blank'>
                                 <FontAwesomeIcon icon={faSquareFacebook} className='text-white' size='xl'/>
                             </a>
@@ -50,11 +76,6 @@ const Tooltip = ( { text, icons, trigger, children } ) =>{
                             </a>
                         </div>
                     </div>
-                </>
-                ) : (
-                <>
-                    <button className="rounded-full w-8 h-8 p-2 bg-slate-100 text-[var(--dark-blue)] hover:bg-slate-500 hover:text-white">{children}</button>
-                    <div className={StyleTooltip.tooltip__container}>{text}</div>
                 </>
             )}
         </div>
